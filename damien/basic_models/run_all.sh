39fbcae37dfcfc6b99b9e3932c46f8bca6d25d9e
Christopher D. Long <galizur@gmail.com>
2018-03-29 20:49:14 -0400

    Aymeric Damien examples with Titan V training times. Using TensorFlow 1.8.

---
+++ damien/basic_models/run_all.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python kmeans.py > kmeans.txt
+python logistic_regression.py > logistic_regression.txt
+python linear_regression_eager_api.py > linear_regression_eager_api.txt
+python nearest_neighbor.py > nearest_neighbor.txt
+python linear_regression.py > linear_regression.txt
+python random_forest.py > random_forest.txt
+python logistic_regression_eager_api.py > logistic_regression_eager_api.txt
